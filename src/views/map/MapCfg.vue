8c8f33d66a2eeaddf5b8e539ae41a068a88fa2e9
Ryan Lee <gxlhybh@gmail.com>
2020-02-26 13:45:47 +0800

    init ts version vuehr

---
+++ src/views/map/MapCfg.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+    import {Component} from "vue-property-decorator";
+    @Component
+    export default class MapConfig extends Vue {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
